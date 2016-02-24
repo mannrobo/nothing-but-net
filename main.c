a6b39b5f7145140007f7bca5ed96ab819c18f64c
MayorMonty <artificalchamber@gmail.com>
2016-02-23 20:08:35 -0500

    Show example of PID loop, will be used to house competition code later

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <pid.c>
+
+/** PID Controllers **/
+static PIDControlled flywheel;
+flywheel.IMEMotor = LeftFlywheel;
+
+static PIDControlled driveTrain;
+driveTrain.IMEMotor = DriveBackLeft;
+task usercontrol() {
+  while (true) {
+    PIDUpdate(&flywheel);
+    flywheel.target = 200;
+    motor[LeftFlywheel] = flywheel.drive;
+    motor[RightFlywheel] = flywheel.drive;
+  }
+}
